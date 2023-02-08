24097c7fe6382c06adc9c99380abad88f56035b6
ankhu1610 <87668370+ankhu1610@users.noreply.github.com>
2023-02-08 20:39:28 +0530

    Add files via upload

---
+++ .github/workflows/adder.java
@@ -0,0 +1,22 @@
+class adder 
+{
+	static int add(int a,int b)
+	{
+		return a+b;
+	}
+	static int add(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+	void main()
+	{
+		System.out.println("hellow");
+
+	public static void main(String[] args)
+	{	
+		System.out.println(add(4,5));
+		System.out.println(add(4,5,6));
+	}
+}
+
+
